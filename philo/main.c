ceaeb02ef5c7e8639c04c17affd2411e83c7cb88
Aymen El Arrouji <aymane.arrouji@gmail.com>
2025-03-12 00:05:59 +0000

    tkhrbi9

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: ayel-arr <ayel-arr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 08:24:29 by ayel-arr          #+#    #+#             */
-/*   Updated: 2025/03/05 01:09:10 by ayel-arr         ###   ########.fr       */
+/*   Updated: 2025/03/12 00:05:18 by ayel-arr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,66 @@ void	*philosopher(void	*arg)
 {
 	t_philo				*philo;
 	pthread_mutex_t		lock;
-	int					right;
-	int					left;
+	int					forks_index[2];
+	struct timeval		tv;
+	static int			ate;
+	suseconds_t			last_time;
 
 	philo = (t_philo *)arg;
 	if (philo->number == 0)
-		left = philo->args[0] - 1;
+		forks_index[0] = philo->args[0] - 1;
 	else
-		left = philo->number - 1;
-	right = philo->number;
+		forks_index[0] = philo->number - 1;
+	forks_index[1] = philo->number;
 	pthread_mutex_init(&lock, NULL);
-	if (philo->forks[philo->number - 1] == 'a'
-		&& philo->forks[philo->number] == 'a')
+	if (philo->forks[forks_index[0]] == 'a' && philo->forks[forks_index[1]] == 'a')
 	{
-		printf("%i is eating\n", philo->number + 1);
-		pthread_mutex_lock(&lock);	
-		philo->forks[philo->number - 1] = 'u';
-		philo->forks[philo->number] = 'u';
+		(printf("%i is eating\n", philo->number + 1), pthread_mutex_lock(&lock));
+		ate++;
+		philo->forks[forks_index[1]] = 'u';
+		philo->forks[forks_index[0]] = 'u';
+		(pthread_mutex_unlock(&lock), usleep(philo->args[2] * 1000));
+		pthread_mutex_lock(&lock);
+		philo->forks[forks_index[1]] = 'a';
+		philo->forks[forks_index[0]] = 'a';
 		pthread_mutex_unlock(&lock);
-		usleep(philo->args[2] * 1000);	
 		printf("%i is sleeping\n", philo->number + 1);
+		usleep(philo->args[3] * 1000);
+		printf("%i is thinking\n", philo->number + 1);
 	}
-	free(arg);
-	return (NULL);
+	gettimeofday(&tv, NULL);
+	while (philo->forks[forks_index[0]] != 'a' && philo->forks[forks_index[1]] != 'a')
+	{
+		last_time = tv.tv_usec;
+		gettimeofday(&tv, NULL);
+		if (tv.tv_usec - last_time > philo->args[1] * 1000)
+			return (philo->args[5] = 0, NULL);
+	}
+	philosopher(arg);
+	return (free(arg), NULL);
 }
 
 void	death_check(int *args)
 {
 	while (args[5] == args[0] && args[4] != 0)
 		continue ;
+}
+
+void	make_threads(pthread_t	*threads, int *args, char *forks, char j)
+{
+	int			i;
+	t_philo		*tmp;
+
+	i = j;
+	while (i < args[0])
+	{
+		tmp = malloc(sizeof(t_philo));
+		tmp->args = args;
+		tmp->forks = forks;
+		tmp->number = i;
+		pthread_create(&threads[i], NULL, philosopher, tmp);
+		i += 2;
+	}
 }
 
 //	args[0] = number_of_philosophers
@@ -60,40 +91,16 @@ int	main(int argc, char **argv)
 	int			*args;
 	char		*forks;
 	pthread_t	*threads;
-	t_philo		*tmp;
-	int			i;
 
-	i = 0;
 	args = get_args(argc, argv);
 	if (args == NULL)
 		return (error(), 0);
 	forks = malloc(args[0] * sizeof(char));
 	threads = malloc(args[0] * sizeof(pthread_t));
 	memset(forks, 'a', (args[0] * sizeof(char)));
-	while (i < args[0])
-	{
-		if (i % 2 == 0)
-		{
-			tmp = malloc(sizeof(t_philo));
-			tmp->args = args;
-			tmp->forks = forks;
-			tmp->number = i;
-			pthread_create(&threads[i], NULL, philosopher, tmp);
-		}
-		i++;
-	}
-	i = 0;
-	while (i < args[0])
-	{
-		if (i % 2 != 0)
-		{
-			tmp = malloc(sizeof(t_philo));
-			tmp->args = args;
-			tmp->forks = forks;
-			tmp->number = i;
-			pthread_create(&threads[i], NULL, philosopher, tmp);
-		}
-		i++;
-	}
+	// TODO: if there is one philospher.
+	make_threads(threads, args, forks, 0);
+	make_threads(threads, args, forks, 1);
+	free(threads);
 	return (death_check(args), free(args), free(forks), 0);
 }
